44dbbb60640ff020f141619f481135371d6a5791
AxTerium <kirill.kazancev.2001@mail.ru>
2025-05-19 10:01:14 +0700

    Initial commit

--- src/test/java/ru/projects/cart_service/CartServiceApplicationTests.java
+++ src/test/java/ru/projects/cart_service/CartServiceApplicationTests.java
@@ -3,7 +3,7 @@ package ru.projects.cart_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CartServiceApplication.class)
 class CartServiceApplicationTests {
 
     @Test
