4e2ec145c3f31bd121677b77e3ced7e76f5bd8ef
AxTerium <kirill.kazancev.2001@mail.ru>
2025-07-09 18:23:02 +0700

    cors and open api auth

--- src/main/java/ru/projects/cart_service/controller/CartController.java
+++ src/main/java/ru/projects/cart_service/controller/CartController.java
@@ -1,5 +1,6 @@
 package ru.projects.cart_service.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,29 +24,34 @@ public class CartController {
         return ResponseEntity.ok(cartService.validateCartItems(cartDto));
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping
     public ResponseEntity<Set<ValidatedCartItemDto>> getCart(@AuthenticationPrincipal UUID userId) {
         return ResponseEntity.ok(cartService.getCartItems(userId));
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping("/merge")
     public ResponseEntity<String> mergeCart(@RequestBody CartDto cartDto, @AuthenticationPrincipal UUID userId) {
         cartService.mergeCarts(cartDto, userId);
         return ResponseEntity.ok("Carts successfully merged");
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping("/add")
     public ResponseEntity<String> addCartItem(@RequestBody CartItemRequestDto cartItemRequestDto, @AuthenticationPrincipal UUID userId) {
         cartService.addCartItem(cartItemRequestDto.productVariationId(), userId);
         return ResponseEntity.ok("Item successfully added to the cart");
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping("/subtract")
     public ResponseEntity<String> subCartItem(@RequestBody CartItemRequestDto cartItemRequestDto, @AuthenticationPrincipal UUID userId) {
         cartService.subtractCartItem(cartItemRequestDto.productVariationId(), userId);
         return ResponseEntity.ok("Item successfully subtracted from the cart");
     }
 
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping("/clear")
     public ResponseEntity<String> clearCart(@AuthenticationPrincipal UUID userId) {
         cartService.clearCart(userId);
